d485073c19ccac1c805a083cc75a6b7fa81ee2da
PushKaf <pushkal.kafley@gmail.com>
2022-08-21 15:18:23 -0400

    updated comments

--- firefox.py
+++ firefox.py
@@ -9,32 +9,44 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+#-----------------------------------------
+# Main Broswer class for all actions 
+# performed within the browser.
+#-----------------------------------------
 class Browser():
     def __init__(self, url, show=True) -> None:
         self.URL = url
 
         options = Options()
 
+        # Setting the browser to headless ensures it dosent open a window, but runs without one
         if not show:
             options.add_argument("--headless")
 
         #get the geckodriver used for firefox 
         serv=Service(GeckoDriverManager().install())
 
+        #Adding the options & installing the adblocker addon
         self.firefox = webdriver.Firefox(service=serv, options=options)
         self.firefox.implicitly_wait(10)
         self.firefox.install_addon("adblockULT.xpi")
 
         self.firefox.get(self.URL) 
 
+        #closes the tab opened by the adblocker
         self.switchToTab(-1)
         self.firefox.close()
         self.switchToTab(0)
 
         self.startTime = time.time()
+
         #waits 1 second in between play and pause
         self.timeToPause = 1
 
+    #-----------------------------------------
+    # Using a js script, it presses the
+    # "Next Video" button on the video
+    #-----------------------------------------
     def next(self):
         CLASS = "ytp-next-button"
 
@@ -46,6 +58,10 @@ class Browser():
         except Exception: 
             print(f"Couldnt click the next button, class: {CLASS}")
 
+    #-----------------------------------------
+    # Uses a js script to press the 
+    # Next Video button
+    #-----------------------------------------
     def previous(self):
         CLASS = "ytp-prev-button"
 
@@ -57,6 +73,10 @@ class Browser():
         except Exception:
             print(f"Couldnt click the previous button, class: {CLASS}")
 
+    #-----------------------------------------
+    # Presses the Enter key to play or pause
+    # the video depending on the cur state
+    #-----------------------------------------
     def play(self):
         endTime = time.time()
 
@@ -74,12 +94,14 @@ class Browser():
 
             self.startTime = time.time()
 
+    #-----------------------------------------
+    # Switches to the given tab
+    #-----------------------------------------
     def switchToTab(self, tabNum):
         self.firefox.switch_to.window(self.firefox.window_handles[tabNum])
 
+    #-----------------------------------------
+    # Closes the current active tab
+    #-----------------------------------------
     def closeTab(self):
         self.firefox.close()
-
-if __name__ == '__main__':
-    x = Browser("https://www.youtube.com/watch?v=-thh5_bpGGY&list=RD-thh5_bpGGY&start_radio=1")
-    x.scrub(20)
